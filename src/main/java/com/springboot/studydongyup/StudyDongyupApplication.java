db89cbe938ce0bcafcd2b9c4e1eef96055456327
dlehdduq22 <dlehdduq22@naver.com>
2022-07-12 21:00:00 +0900

    init commit

---
+++ src/main/java/com/springboot/studydongyup/StudyDongyupApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studydongyup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyDongyupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyDongyupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studydongyup/StudyDongyupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studydongyup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDongyupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
